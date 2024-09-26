f8687aedbb488b4c52b961a479c7255ef9d3db1b
anavaradha-sankar <68526592+anavaradha-sankar@users.noreply.github.com>
2024-09-26 20:21:53 +0200

    Removed unwanted code in main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -8,38 +8,3 @@ terraform {
   
   required_version = ">= 0.12"
 }
-
-
-
-provider "azurerm" {
-  features {}
-}
-
-# Create a resource group
-resource "azurerm_resource_group" "resource_group" {
-  name     = "anavaradhaCodes" # Change this to your desired resource group name
-  location = "West Europe"                  # Choose your preferred region
-}
-
-
-# Create a storage account
-resource "azurerm_storage_account" "storage_account" {
-  name                     = "anavaradhacodes"
-  resource_group_name      = azurerm_resource_group.resource_group.name
-  location                 = azurerm_resource_group.resource_group.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-
-  static_website {
-    index_document     = "index.html"
-    error_404_document = "index.html"
-  }
-}
-
-resource "azurerm_storage_blob" "blob" {
-  name = "index.html"
-  storage_account_name = azurerm_storage_account.storage_account.name
-  storage_container_name = "$web"
-  type = "Block"
-}
