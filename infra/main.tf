48a055e7cb2d43be50a16dd8e9428f5a5de2ce40
Anavaradha Sankar <anavaradha.sankar@gmail.com>
2024-09-26 17:21:29 +0200

    Implemented Build workflow

--- infra/main.tf
+++ infra/main.tf
@@ -2,21 +2,30 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "anavaradhacodes_rg" {
-  name     = "anavaradhaCodesResourceGroup"  # Change this to your desired resource group name
-  location = "West Europe"       # Choose your preferred region
+# Create a resource group
+resource "azurerm_resource_group" "resource_group" {
+  name     = "anavaradhaCodes" # Change this to your desired resource group name
+  location = "West Europe"                  # Choose your preferred region
 }
 
-resource "azurerm_static_site" "anavaradhacodes_site" {
-  name                = "anavaradha-codes-site"  # Change this to your desired app name
-  resource_group_name = azurerm_resource_group.my_resource_group.name
-  location            = azurerm_resource_group.my_resource_group.location
+# Create a storage account
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "anavaradhacodes"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
 
-  sku_tier            = "Free"                    # Use Free tier
-  repository_url      = "https://github.com/YourGitHubUser/YourRepo"  # Update with your GitHub repo URL
-  branch              = "main"
-  app_location        = "/"                         # Root directory for a React app
+  static_website {
+    index_document     = "index.html"
+    error_404_document = "index.html"
+  }
+}
 
-  # Specify the location of the build output folder (usually 'build' for React apps)
-  output_location     = "build"                    # Change this if your build folder is different
+resource "azurerm_storage_blob" "blob" {
+  name = "index.html"
+  storage_account_name = azurerm_storage_account.storage_account.name
+  storage_container_name = "$web"
+  type = "Block"
 }
